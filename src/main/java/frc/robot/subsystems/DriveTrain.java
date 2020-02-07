a42f008e19b1a4939c6673a244be2c10de63bc61
AndreSDin <44779129+AndreSDin@users.noreply.github.com>
2020-02-07 14:54:56 -0500

    Inverted Motor to correct orientation

--- src/main/java/frc/robot/subsystems/DriveTrain.java
+++ src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -76,7 +76,7 @@ public class DriveTrain extends SubsystemBase {
   }
 
   public void drive(double left, double right) {
-    drive.arcadeDrive(left * 0.5, right * 0.5, false);
+    drive.arcadeDrive((left * 0.5) * -1, (right * 0.5) * -1, false);
 
   }
 
